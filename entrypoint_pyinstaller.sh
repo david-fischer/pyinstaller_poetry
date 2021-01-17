d05feb5510cdd1f25b16d699c0dffe3febe63994
David Fischer <d.fischer.git@posteo.de>
2021-01-17 23:27:05 +0100

    add: pre_package_hook

--- entrypoint_pyinstaller.sh
+++ entrypoint_pyinstaller.sh
@@ -2,12 +2,13 @@
 SRC_DIR=$1
 SPEC_FILE=${2:-*.spec}
 OUT_PATH=$3
+pre_package_hook=$4
 #change dir
 cd "$SRC_DIR" || exit
 #install
 poetry config virtualenvs.create false
 poetry add pyinstaller virtualenv
-python -m spacy download pt
+$pre_package_hook
 #build
 pyinstaller --clean -y --distpath "$OUT_PATH" "$SPEC_FILE"
 #change permissions
